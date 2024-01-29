655c89963d2f2bb89bd16c7a8b9fd2c635436a1d
Joonhyuk Sonng <thenoface303@gmail.com>
2024-01-29 22:13:01 +0900

    swea: python: 1206

---
+++ swea/D3/1206.py
@@ -0,0 +1,27 @@
+import sys
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.stdin = open(f"{dir_path}/input.txt", "r")
+input = sys.stdin.readline
+
+for n in range(1, 11):
+    N = int(input())
+    B = list(map(int, input().split()))
+    result = 0
+
+    for i in range(2, N-2):  # 양 끝의 2개는 건물 X
+        # 좌/우로 2개 비교
+        # 좌/우마다 2개 값 중 큰 값만 비교
+        # direction = [-2, -1, 1, 2]
+        
+        left_max_value = max(B[i-2], B[i-1])
+        right_max_value = max(B[i+1], B[i+2])
+
+        if B[i] > left_max_value and B[i] > right_max_value:
+            if left_max_value - right_max_value > 0:
+                result += B[i] - left_max_value
+            else:
+                result += B[i] - right_max_value
+
+    print(f'#{n} {result}')
--- swea/README.md
+++ swea/README.md
@@ -1,5 +1,6 @@
 # SWEA
 ## 2024 January
-| Date | Difficulty | Problem # |
-| --- | --- | --- |
-| 2024-01-27 | D2 | [1974](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5Psz16AYEDFAUq) |
+| Date | Difficulty | Problem # | Language | Memory | Time |
+| --- | --- | --- | --- | --- | --- |
+| 2024-01-27 | D2 | [1974](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5Psz16AYEDFAUq) | Python | 48,664kb | 114ms |
+| 2024-01-29 | D3 | [1206](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV134DPqAA8CFAYh) | Python | 54,064kb | 144ms | 
