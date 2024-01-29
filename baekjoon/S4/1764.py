81bd6292c11d5987597e198cd0fb9c44f994667a
Joonhyuk Sonng <thenoface303@gmail.com>
2024-01-29 13:46:53 +0900

    baekjoon: python: 1764: Improve input method

--- baekjoon/S4/1764.py
+++ baekjoon/S4/1764.py
@@ -2,18 +2,18 @@
 
 import sys
 sys.stdin = open("input.txt", "r")
+input = sys.stdin.readline
 
-N, M  = list(map(int, input().split()))
+N, M  = map(int, input().split())
 
 never_listened = set()
 for i in range(N):
-    never_listened.add(input())
+    never_listened.add(input().strip())
 
 never_seen = set()
 for i in range(M):
-    never_seen.add(input())
+    never_seen.add(input().strip())
 
-# set 연산
 result = list(never_listened & never_seen)
 result.sort()
 
