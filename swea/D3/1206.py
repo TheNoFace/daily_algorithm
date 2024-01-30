01da10566a5fa986f0969f31dec597ee1b925748
Joonhyuk Sonng <thenoface303@gmail.com>
2024-01-30 13:34:14 +0900

    swea: python: 1206: Use slicing for array

--- swea/D3/1206.py
+++ swea/D3/1206.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-sys.stdin = open(f"{dir_path}/input.txt", "r")
+sys.stdin = open(f"{dir_path}/1206.txt", "r")
 input = sys.stdin.readline
 
 for n in range(1, 11):
@@ -15,8 +15,10 @@ for n in range(1, 11):
         # 좌/우마다 2개 값 중 큰 값만 비교
         # direction = [-2, -1, 1, 2]
         
-        left_max_value = max(B[i-2], B[i-1])
-        right_max_value = max(B[i+1], B[i+2])
+        # left_max_value = max(B[i-2], B[i-1])
+        # right_max_value = max(B[i+1], B[i+2])
+        left_max_value = max(B[i-2:i])
+        right_max_value = max(B[i+1:i+3])
 
         if B[i] > left_max_value and B[i] > right_max_value:
             if left_max_value - right_max_value > 0:
