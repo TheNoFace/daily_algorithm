788ca203423939d514ac47f80b85a31efa5ba002
Joonhyuk Sonng <thenoface303@gmail.com>
2024-02-04 18:27:04 +0900

    baekjoon: python: 3986 [2/2]: Rewrite

--- baekjoon/S4/3986.py
+++ baekjoon/S4/3986.py
@@ -17,31 +17,20 @@ for word in words:
     elif len(word) == 2 and word[0] != word[1]:
         continue
 
-    # 시작 글자의 시작 인덱스와 종료 인덱스 도출
-    # 다음 글자의 시작 인덱스와 종료 인덱스 도출
-    # 시작 글자의 범위 안에 다음 글자의 범위가 있으면 좋은 단어
-    first_char = word[0]
-    second_char = 'A' if first_char == 'B' else 'B'
-    char_index = {'A': [], 'B': []}
-
-    for idx, char in enumerate(word):
-        char_index[char].append(idx)
-
-    # 단어 길이는 짝수이므로 비교 가능한 범위는 1/2
-    for i in range(0, len(char_index[first_char]), 2):
-        start, end = char_index[first_char][i], char_index[first_char][i+1]
-        # 첫 글자의 시작과 종료 사이에 있는 비교할 글자의
-        # 시작, 종료 인덱스의 개수가 홀수개면 break
-        # 짝수개면 진행
-        count = 0
-        for value in char_index[second_char]:
-            if start < value < end:
-                count += 1
-
-        if count % 2 != 0:
-            break
+    # 스택 사용
+    # 스택에 있는 글자가 넣을 글자와 같으면 pop
+    # 스택에 있는 글자가 넣을 글자와 다르면 append
+    stack = []
+    for char in word:
+        if stack:
+            if char == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(char)
         else:
-            good += 1
-            break
+            stack.append(char)
+
+    if len(stack) == 0:
+        good += 1
 
 print(good)
