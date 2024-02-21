ea94e62f93a426d2fab088dc7ede97caac4a569c
Joonhyuk Sonng <thenoface303@gmail.com>
2024-02-21 16:33:56 +0900

    baekjoon: python: 10610 [2/2]

--- baekjoon/S4/10610.py
+++ baekjoon/S4/10610.py
@@ -9,29 +9,7 @@ input = sys.stdin.readline
 
 T = 4
 for t in range(1, T+1):
-    N = list(map(str, input().rstrip()))
-    
-    # 완전 탐색: 순열 만들어 30으로 나눠져 떨어지는 가장 큰 수 구하기
-    # 그리디: 가장 큰 숫자를 만들어보고, 나누어 떨어지지 않으면 순열 만들기?
-    def perm_maker(i, k, arr):
-        if i == k:
-            num = int(''.join(arr))
-            result = max_dividable(num)
-            if result != -1:
-                return result
-        else:
-            for j in range(i, k):
-                arr[i], arr[j] = arr[j], arr[i]
-                perm_maker(i+1, k, arr)
-                arr[j], arr[i] = arr[i], arr[j]
-        return -1
-
-    def max_dividable(n):
-        if n % 30 == 0: return n
-        else: return -1
-
-    max_N = int(''.join(sorted(N, reverse=True)))
-    if max_dividable(max_N) == max_N:
-        print(max_N)
-    else:
-        print(perm_maker(0, len(N), N))
+    # 완전 탐색: 순열 만들어 30으로 나눠져 떨어지는 가장 큰 수 구하기 -> recursion error
+    # 그리디: N을 내림차순 정렬 후, 30으로 나뉘어지면 N 반환, 아니면 -1 반환
+    N = int(''.join(sorted(list(map(str, input().rstrip())), reverse=True)))
+    print(N) if N % 30 == 0 else print(-1)
