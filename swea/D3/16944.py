51109093dc25ef34be8a61a92ceb977bc3b0eb9d
Joonhyuk Sonng <thenoface303@gmail.com>
2024-03-19 13:29:22 +0900

    swea: python: 16944 [2/2]: Rewrite

--- swea/D3/16944.py
+++ swea/D3/16944.py
@@ -4,53 +4,64 @@ import sys
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-sys.stdin = open(f'{dir_path}/input.txt', 'r')
+sys.stdin = open(f"{dir_path}/input.txt", "r")
+
+
+def merge_sort(l, r):
+    global count
+
+    # 최소 부분집합까지 나눔
+    if (r - l) == 1:
+        return l, r
+
+    mid = (l + r) // 2
+    left = merge_sort(l, mid)
+    right = merge_sort(mid, r)
+
+    if arr[left[1] - 1] > arr[right[1] - 1]:
+        count += 1
+
+    merge(left, right)
+    return left[0], right[1]
+
+
+def merge(left, right):
+    length = right[1] - left[0]
+    result = [0] * length
+    l, r = left[0], right[0]
+
+    idx = 0
+    while l < left[1] and r < right[1]:
+        if arr[l] < arr[r]:
+            result[idx] = arr[l]
+            l += 1
+            idx += 1
+        else:
+            result[idx] = arr[r]
+            r += 1
+            idx += 1
+
+    if l < left[1]:
+        while l < left[1]:
+            result[idx] = arr[l]
+            l += 1
+            idx += 1
+    elif r < right[1]:
+        while r < right[1]:
+            result[idx] = arr[r]
+            r += 1
+            idx += 1
+
+    # 실제 리스트에 복사
+    for i in range(length):
+        arr[left[0] + i] = result[i]
+
 
 T = int(input())
-for t in range(1, T+1):
+for t in range(1, T + 1):
     N = int(input())
     arr = list(map(int, input().split()))
     count = 0
 
-    def merge_sort(arr):
-        global count
-        # 최소 부분집합까지 나눔
-        if len(arr) == 1:
-            return arr
-        
-        mid = len(arr) // 2
-        left = merge_sort(arr[:mid])
-        right = merge_sort(arr[mid:])
-        
-        if left[-1] > right[-1]:
-            count += 1
-
-        return merge(left, right)
-
-    def merge(left, right):
-        result = [0] * (len(left) + len(right))
-        l = r = 0
-
-        while l < len(left) and r < len(right):
-            if left[l] < right[r]:
-                result[l + r] = left[l]
-                l += 1
-            else:
-                result[l + r] = right[r]
-                r += 1
-
-        if l < len(left):
-            while l < len(left):
-                result[l + r] = left[l]
-                l += 1
-        elif r < len(right):
-            while r < len(right):
-                result[l + r] = right[r]
-                r += 1
-        else:
-            print(f'{l}/{len(left)}, {r}/{len(right)}')
-
-        return result
-
-    arr = merge_sort(arr)
-    print(f'#{t} {arr[N // 2]} {count}')
+    merge_sort(0, N)
+    print(f"#{t} {arr[N // 2]} {count}")
