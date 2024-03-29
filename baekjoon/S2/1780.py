3c0a0cd4a6bee400bf6f5791c58e2ce812ce2372
Joonhyuk Sonng <thenoface303@gmail.com>
2024-03-29 16:22:21 +0900

    baekjoon: python: 1780 [2/2]: Rewrite

--- baekjoon/S2/1780.py
+++ baekjoon/S2/1780.py
@@ -1,37 +1,36 @@
 import sys
+
 input = sys.stdin.readline
 
 N = int(input())
 grid = [list(map(int, input().split())) for _ in range(N)]
 result = [0, 0, 0]
 
-visited = [[0] * N for _ in range(N)]
-def search(div):
+
+def search(sr, sc, div):
+    global result
+
     if div == 0:
         return
 
-    for sr in range(0, N, div):
-        for sc in range(0, N, div):
-            init = grid[sr][sc]
-            visit_stack = []
-            count = 0
-            if not visited[sr][sc]:
-                for r in range(sr, sr + div):
-                    for c in range(sc, sc + div):
-                        if init == grid[r][c]:
-                            visit_stack.append((r, c))
-                            count += 1
-                            init = grid[r][c]
-                        else:
-                            search(div // 3)
-                            return
-                else:
-                    for _ in range(len(visit_stack)):
-                        r, c = visit_stack.pop()
-                        visited[r][c] = 1
-                    result[init] += 1
+    init = grid[sr][sc]
+    count = 0
+    for r in range(sr, sr + div):
+        for c in range(sc, sc + div):
+            if init == grid[r][c]:
+                count += 1
+            else:
+                break
+    # 다 세본 다음 개수가 div가 아니면 재귀 진입
+    if count != div**2:
+        for r in range(sr, sr + div, div // 3):
+            for c in range(sc, sc + div, div // 3):
+                search(r, c, div // 3)
+    else:
+        result[init] += 1
     return
 
-search(N)
+
+search(0, 0, N)
 for i in range(-1, 2):
     print(result[i])
