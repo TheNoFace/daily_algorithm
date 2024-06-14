57742a528e1e9dbff17434c9e8589d984277b5bb
Joonhyuk Sonng <thenoface303@gmail.com>
2024-06-14 19:34:42 +0900

    baekjoon: python: 14550

---
+++ baekjoon/G5/14550.py
@@ -0,0 +1,40 @@
+# https://www.acmicpc.net/problem/14550
+
+import sys
+
+input = sys.stdin.readline
+
+
+while True:
+    input_str = list(map(int, input().split()))
+    if len(input_str) == 1 and input_str[0] == 0:
+        break
+
+    N, S, T = map(int, input_str)
+    board = [0]  # 시작점 설정
+    dp = [[-10000 * N] * (N + 1) for _ in range(T)]
+
+    while len(board) != N + 1:
+        board.extend(list(map(int, input().split())))
+
+    board.append(0)  # 종료점 설정
+    result = set()
+
+    # 초기값 설정
+    for i in range(1, S + 1):
+        dp[0][i] = board[i]
+
+    # 직전 턴에 도달한 각 인덱스마다 1 ~ S 눈금의 주사위를 굴림
+    for t in range(1, T):
+        for idx in range(t, t * S + 1):
+            for s in range(1, S + 1):
+                if idx > N:
+                    continue
+
+                # 별 도달
+                if idx + s > N:
+                    result.add(dp[t - 1][idx])
+                else:
+                    dp[t][idx + s] = max(dp[t - 1][idx] + board[idx + s], dp[t][idx + s])
+
+    print(max(result))
